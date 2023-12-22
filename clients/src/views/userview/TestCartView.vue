61f12b9a4341e60e2b80a3b9edcd159084a40b5d
sonharang <bhson3@naver.com>
2023-12-22 17:40:48 +0900

    harang

--- clients/src/views/userview/TestCartView.vue
+++ clients/src/views/userview/TestCartView.vue
@@ -11,8 +11,8 @@
         </li>
       </ul>
     </div>
+    <h1>업체명</h1>
     <table class="cart_list">
-      <h1>업체명</h1>
       <thead>
         <tr>
           <td><input type="checkbox" id="allcheck" /></td>
@@ -25,12 +25,75 @@
           </td>
         </tr>
       </thead>
-      <tbody></tbody>
+      <tbody>
+        <tr
+          class="cart_list_detail"
+          v-for="products in productList"
+          :key="products.product_name"
+        >
+          <td>
+            <input
+              type="checkbox"
+              class="chk"
+              name="product"
+              value="가격"
+              onclick="checkfunction()"
+            />
+          </td>
+          <td>
+            <input
+              type="checkbox"
+              id="pno"
+              name="pno"
+              value="상품번호"
+              style="display: none"
+            />
+          </td>
+          <td>
+            <a href=""><img src="" /></a>
+          </td>
+          <td>
+            <a href="">상품설명</a
+            ><span class="cart_list_smartstore">스마트스토어</span>
+            <br />
+            <span class="price">{{ products.product_price }}</span>
+          </td>
+          <td class="cart_list_option">
+            <p>모델명 : {{ products.product_name }}</p>
+            <p>수량 : {{ products.product_stock }}</p>
+            <input
+              type="button"
+              value="▲"
+              class="cart_list_optionbtn"
+              onclick="upfunction()"
+            />
+            <input
+              type="button"
+              value="▼"
+              class="cart_list_optionbtn"
+              onclick="downfunction()"
+            />
+            <input
+              type="button"
+              value="상품 삭제"
+              class="cart_list_optionbtn"
+              onclick="delfunction()"
+            />
+          </td>
+          <td>
+            <span class="price">{{
+              products.product_price * products.product_stock
+            }}</span>
+            <br />
+          </td>
+          <td>{{ products.delivery_fee }}</td>
+        </tr>
+      </tbody>
       <tfoot>
         <tr>
           <td colspan="3"></td>
           <td>
-            상품금액 :
+            체크된 상품금액 합계:
             <h4 id="total"></h4>
           </td>
           <td>
@@ -38,8 +101,8 @@
             <h4 id="price"></h4>
           </td>
           <td>
-            총 금액 :
-            <h4 id="totalPrice"></h4>
+            총 금액 = 상품금액 합계 + 배송비 :
+            <h3 id="totalPrice"></h3>
           </td>
           <td></td>
           <td></td>
@@ -47,13 +110,55 @@
         </tr>
       </tfoot>
     </table>
+    <div class="cart_mainbts">
+      <button
+        class="cart_bigorderbtn left"
+        type="button"
+        onclick="location.href='main.do'"
+      >
+        쇼핑계속하기
+      </button>
+      <button
+        class="cart_bigorderbtn right"
+        type="button"
+        id="tess"
+        onclick="paymentfunction()"
+      >
+        주문하기
+      </button>
+    </div>
   </div>
 </template>
 <script>
-import axios from "axios";
 export default {
   data() {
-    return;
+    return {
+      productList: [],
+    };
+  },
+  creted() {
+    this.test();
+  },
+  methods: {
+    async test() {
+      // A업체
+      this.productList.push(
+        {
+          product_no: 1,
+          product_name: "칫솔",
+          product_price: 15000,
+          product_stock: 2,
+          delivery_fee: 3000,
+        },
+        {
+          product_no: 2,
+          product_name: "치약",
+          product_price: 10000,
+          product_stock: 5,
+          delivery_fee: 3000,
+        }
+      );
+    },
   },
 };
 </script>
