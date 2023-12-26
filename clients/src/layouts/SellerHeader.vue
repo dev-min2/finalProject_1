6cfc259b3b769a5de8a82f2e55ac0949ea83157e
pks12341 <parkksk95@naver.com>
2023-12-26 10:39:19 +0900

    committttt

--- clients/src/layouts/SellerHeader.vue
+++ clients/src/layouts/SellerHeader.vue
@@ -15,7 +15,7 @@
     <body class="sb-nav-fixed">
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <!-- Navbar Brand-->
-            <a class="navbar-brand ps-3" href="index.html"><b>마이 디어 펫</b></a>
+             <router-link class="nav-link" to="/main"><b>디어 마이 펫</b></router-link>
             <!-- Sidebar Toggle-->
             <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
             <!-- Navbar Search-->
