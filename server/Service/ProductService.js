31add5771617482bcf774307ef76beb0886c5266
taegyeognk <gofus2498@naver.com>
2023-12-31 18:39:46 +0900

    pagedto

--- server/Service/ProductService.js
+++ server/Service/ProductService.js
@@ -1,5 +1,6 @@
 const productDAO = require("../DAO/product/ProductDAO");
 const categoryDAO = require("../DAO/product/CategoryDAO");
+const pageDTO = require("../commonModule/PageDTO");
 
 class ProductService {
     constructor() {
@@ -16,32 +17,40 @@ class ProductService {
 
     }
     //검색한 상품리스트
-    async getSearchProductList(search, ptype,pageno) {
-        let result = await productDAO.selectSearchProductQuery(search, ptype,pageno);
-        console.log(search, ptype,pageno);
+    async getSearchProductList(search, ptype, pageno) {
+        let result = await productDAO.selectSearchProductQuery(search, ptype, pageno);
+        console.log(search, ptype, pageno);
         return result;
     }
     //카테고리 정보
-    async getCategoryData(){
+    async getCategoryData() {
         let result = await categoryDAO.selectCategoryDataQuery();
         console.log(result);
         return result;
     }
     // 카테고리 검색한 리스트
-    async getCategorySearch(cno, ptype, pageno){
-        let result = await categoryDAO.selectCategorySearchQuery(cno,ptype,pageno);
-        console.log(cno,ptype,pageno);
+    async getCategorySearch(cno, ptype, pageno) {
+        let result = await categoryDAO.selectCategorySearchQuery(cno, ptype, pageno);
+        console.log(cno, ptype, pageno);
         return result;
     }
     //검색한 상품 결과 개수
-    async getSearchResultCnt(search,ptype){
+    async getSearchResultCnt(pageno) {
+        const result = await productDAO.selectSearchProductQuery(pageno);
+        const countResult = await productDAO.selectSearchResultProductQuery();
 
+        const pageDTO = new pageDTO(countResult[0].CNT, Number(pageno), 10);
+        const resResult = {
+            selectResult: result,
+            pageDTO: pageDTO
+        }
+        return resResult;
     }
     // 카테고리 상품 개수
-    async getCategoryProductCnt(cno, ptype){
-        
+    async getCategoryProductCnt(pageno) {
+
     }
-    
+
     //트랜잭션 예시코드임
     // async completePayment() {
     //     const connection = await getConnection();
