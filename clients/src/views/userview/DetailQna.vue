12bffbc9f64506d542bcdaf7f2e65b936b81446c
sonharang <bhson3@naver.com>
2024-01-11 01:03:39 +0900

    집커밋

--- clients/src/views/userview/DetailQna.vue
+++ clients/src/views/userview/DetailQna.vue
@@ -4,7 +4,7 @@
 		<div class="container-fluid">
 			<form action="modifyUserQnaForm.do" name="userQnaDetailForm" method="post" style=text-align:center;>
 				<input type="hidden" name="qnaNo" value="">
-				<h3>문의글</h3>
+				<h3 style="font-weight : bold">문의글</h3>
 				<br><hr>
 				<table class="table" border="1">
 					<tr>
